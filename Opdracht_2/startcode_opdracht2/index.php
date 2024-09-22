48f652f32b9328719ad7e73ab87069d90f41ed22
nickker3 <nickbakker68@gmail.com>
2024-09-22 11:27:40 +0200

    opdracht 2 gestart

---
+++ Opdracht_2/startcode_opdracht2/index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
+define('FILES_PATH/', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+define('/startcode_opdracht2/views', $root . 'views' . DIRECTORY_SEPARATOR);
+
+/* HIER CODE (zie de instructies op Blackboard) */
+/home/unknown/Documents/School Semester1
